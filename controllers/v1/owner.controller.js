f1587c94a5cb0acce3c334e02a70832d4dae3499
ME.AT <meatish2008@gmail.com>
2025-06-18 11:17:13 +0330

    feat(owner.controller.js): create feature for remove onwer from website (only admin access to this route) and also create feature for update the owner's information

--- controllers/v1/owner.controller.js
+++ controllers/v1/owner.controller.js
@@ -33,3 +33,59 @@ exports.register = async (req, res) => {
     }
   }
 };
+
+exports.remove = async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (isValidObjectId(id)) {
+      return res.status(422).json({ message: "Id is not valid" });
+    }
+
+    const deletedOwner = await ownerModel.findByIdAndDelete({ _id: id });
+
+    if (!deletedOwner) {
+      return res.status(403).json({ message: "The owner not founds" });
+    }
+
+    return res.status(200).json({ message: "Owner deleted successfully" });
+  } catch (error) {
+    if (error) {
+      throw error;
+    }
+  }
+};
+
+exports.update = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (isValidObjectId(id)) {
+      return res.status(422).json({ message: "Id is not valid" });
+    }
+    const { error } = registerOwnerValidator(req.body);
+    if (error) {
+      return res
+        .status(422)
+        .json({ message: "Validation failed", details: error.details });
+    }
+
+    const { city } = req.body;
+
+    const updatedUser = await ownerModel.findByIdAndUpdate(
+      { _id: id },
+      { city }
+    );
+
+    if (!updatedUser) {
+      return res.status(409).json({ message: "owner not found" });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "The owner's information updated successfully" });
+  } catch (error) {
+    if (error) {
+      throw error;
+    }
+  }
+};
--- routers/v1/owner.router.js
+++ routers/v1/owner.router.js
@@ -10,6 +10,12 @@ router
   .route("/")
   .get(authMiddleware, isAdminMiddlewares, ownerController.getAll);
 
-router.route("/").post(authMiddleware, ownerController.register)
+router.route("/").post(authMiddleware, ownerController.register);
+
+router
+  .route("/:id")
+  .delete(authMiddleware, isAdminMiddlewares, ownerController.remove);
+
+router.route("/:id").put(authMiddleware, ownerController.update);
 
 module.exports = router;
