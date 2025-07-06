f63f2dd92286db5d28a6fcec7499ec008ea845c1
ME.AT <meatish2008@gmail.com>
2025-07-06 20:35:08 +0330

    fix(user.controller.ts): fix small mistake fix(user.controller.ts): fix strange err in Request type

--- src/controllers/v1/user.controller.ts
+++ src/controllers/v1/user.controller.ts
@@ -16,9 +16,16 @@ import { generateRefreshToken } from "../../utils/auth.js";
  * @return res
  */
 export const getAll = async (req: Request, res: Response): Promise<void> => {
-  const users = await userModel.find({}).select("-password");
+  try {
+    const users = await userModel.find({}).select("-password");
 
-  res.json(users);
+    res.json(users);
+    return;
+  } catch (error) {
+    if (error) {
+      throw error;
+    }
+  }
 };
 
 /**
@@ -35,17 +42,20 @@ export const banUser = async (req: Request, res: Response): Promise<void> => {
     // Validate id
     if (isValidObjectId(id)) {
       res.status(422).json({ message: "The id not valid" });
+      return;
     }
 
     // Find user for ban
     const mainUser = await userModel.findOne({ _id: id }).lean();
     if (!mainUser) {
       res.status(403).json({ message: "User not found" });
+      return;
     }
     // Ban user process
     await userModel.findByIdAndUpdate({ _id: mainUser!._id }, { isBan: true });
 
     res.status(200).json({ message: "The user baned successfully" });
+    return;
   } catch (error) {
     if (error) {
       throw error;
@@ -67,15 +77,18 @@ export const remove = async (req: Request, res: Response): Promise<void> => {
     // Validate id
     if (isValidObjectId(id)) {
       res.status(422).json({ message: "Id is not valid" });
+      return;
     }
 
     const removeUser = await userModel.findByIdAndDelete({ _id: id });
 
     if (!removeUser) {
       res.status(403).json({ message: "User not found" });
+      return;
     }
 
     res.status(200).json({ message: "User delete successfully" });
+    return;
   } catch (error) {
     if (error) {
       throw error;
@@ -92,14 +105,19 @@ export const remove = async (req: Request, res: Response): Promise<void> => {
  * @returns res
  */
 export const updateInfo = async (
-  req: AuthenticationRequest,
+  req: Request,
   res: Response
 ): Promise<void> => {
   try {
+
+    const typedReq = req as AuthenticationRequest
+
+    // Validate data with Zod
     const validationResult = userValidator.register(req.body);
 
     if (!validationResult.success) {
       res.status(422).json({ error: validationResult.error.errors });
+      return;
     }
 
     const { name, username, email, phone, password } = req.body;
@@ -113,14 +131,14 @@ export const updateInfo = async (
     // Update user's info
     const updateUser = await userModel
       .findByIdAndUpdate(
-        { _id: req.user._id },
+        { _id: typedReq.user._id },
         {
           name,
           username,
           email,
           phone,
           password: hashedPassword,
-          role: req.user.role,
+          role: typedReq.user.role,
           refreshToken: newRefreshToken,
         }
       )
@@ -130,6 +148,7 @@ export const updateInfo = async (
     res.cookie("refresh_token", newRefreshToken, { httpOnly: true });
 
     res.status(200).json({ updateUser });
+    return;
   } catch (error) {
     if (error) {
       throw error;
@@ -154,19 +173,22 @@ export const changeRole = async (
     // Validate id
     if (isValidObjectId(id)) {
       res.status(422).json({ message: "Id is not valid" });
+      return;
     }
     // Find user to change role
     const user = await userModel.findOne({ _id: id });
     if (!user) {
       res.status(403).json({ message: "User not found" });
+      return;
     }
     // If user's role is admin, it change to user and on the contrary
-    let newRole = user.role === "ADMIN" ? "USER" : "ADMIN";
+    let newRole = user!.role === "ADMIN" ? "USER" : "ADMIN";
 
     // Set new role in database
     await userModel.findByIdAndUpdate({ _id: id }, { role: newRole });
 
     res.status(200).json({ message: "User's role changed successfully" });
+    return;
   } catch (error) {
     if (error) {
       throw error;
