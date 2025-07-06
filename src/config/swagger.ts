c3c4e6b59e1051fb304661042c5255a64fe13aae
ME.AT <meatish2008@gmail.com>
2025-07-06 21:08:55 +0330

    fix(swagger.ts): fix route for show apis

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,24 +1,26 @@
 import swaggerJsDoc from "swagger-jsdoc";
-import swaggerUi from "swagger-ui-express"
-import path from "path"
+import swaggerUi from "swagger-ui-express";
+import { fileURLToPath } from 'url';
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Swagger configuration
-const configSwagger = (app: any) => {
+const configSwagger = (app:any) => {
   const swaggerSpec = swaggerJsDoc({
     swaggerDefinition: {
       openapi: "3.0.1",
       info: {
         title: "Hotel project - ME-AT",
         version: "1.0.0",
-        description:
-          "A RESTful API named meat-hotel using Node.js + express.js + MongoDB.",
+        description: "A RESTful API named meat-hotel using Node.js + express.js + MongoDB.",
       },
       servers: [{ url: "http://localhost:4000" }],
     },
-    apis: [path.join(path.dirname("../routers/v1/**/*.js"))],
+    apis: [path.resolve(__dirname, "../routers/v1/**/*.js")], // absolute path for swagger-jsdoc
   });
-  // Send request to this route for get api document
   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 };
 
-export default configSwagger
+export default configSwagger;
