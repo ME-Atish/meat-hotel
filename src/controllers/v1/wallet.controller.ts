a18e414c463d0cabd5417f24de4fb2650491890f
ME.AT <meatish2008@gmail.com>
2025-07-06 20:38:11 +0330

    fix(wallet.controller.ts): fix small mistake fix(wallet.controller.ts): fix Reqeust type err

--- src/controllers/v1/wallet.controller.ts
+++ src/controllers/v1/wallet.controller.ts
@@ -1,19 +1,23 @@
-import { Response } from "express";
+import { Request,Response } from "express";
 import AuthenticationRequest from "../../utils/authReq.js";
 
 import walletModel from "../../models/wallet.model.js";
 import * as walletValidator from "../../utils/validators/wallet.validator.js";
 
 export const increase = async (
-  req: AuthenticationRequest,
+  req: Request,
   res: Response
 ): Promise<void> => {
   try {
+
+    const typedReq = req as AuthenticationRequest
+
     // Validate data with Zod
     const validationResult = walletValidator.increase(req.body);
 
     if (!validationResult.success) {
       res.status(422).json({ error: validationResult.error.errors });
+      return;
     }
 
     const { amount } = req.body;
@@ -21,7 +25,7 @@ export const increase = async (
     // Increase the account charge
     await walletModel.findOneAndUpdate(
       {
-        userId: req.user._id,
+        userId: typedReq.user._id,
       },
       {
         amount,
@@ -29,6 +33,7 @@ export const increase = async (
     );
 
     res.status(200).json({ message: "Account successfully charged." });
+    return;
   } catch (error) {
     if (error) {
       throw error;
@@ -37,39 +42,46 @@ export const increase = async (
 };
 
 export const decrease = async (
-  req: AuthenticationRequest,
+  req: Request,
   res: Response
 ): Promise<void> => {
   try {
+
+    const typedReq = req as AuthenticationRequest
+
     // Validate data with Zod
     const validationResult = walletValidator.increase(req.body);
 
     if (!validationResult.success) {
       res.status(422).json({ error: validationResult.error.errors });
+      return;
     }
 
     const { amount } = req.body;
     // Find wallet for deducted amount
-    const findWallet = await walletModel.findOne({ userId: req.user._id });
+    const findWallet = await walletModel.findOne({ userId: typedReq.user._id });
     // Check if the amount being sent is less than the account balance.
     if (findWallet!.amount < amount) {
       res.status(406).json({
         message: "The amount to be deducted is less than the account balance.",
       });
+      return;
     }
     // Decrease amount
     const deductedAmount = findWallet!.amount - amount;
     // Update model
     const updateAmount = await walletModel.findOneAndUpdate(
-      { userId: req.user._id },
+      { userId: typedReq.user._id },
       { amount: deductedAmount }
     );
 
     if (!updateAmount) {
       res.status(500).json({ message: "Something wrong..." });
+      return;
     }
 
     res.status(200).json({ message: "The amount was successfully deducted." });
+    return;
   } catch (error) {
     if (error) {
       throw error;
