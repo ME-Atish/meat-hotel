f51c6be5d7b67dc40a1e73185fd269ad2f5b5c51
ME.AT <meatish2008@gmail.com>
2025-08-04 12:45:20 +0330

    refactor(search.controller.ts): refactor search opreation to MySql (via sequelize)

--- src/controllers/v1/search.controller.ts
+++ src/controllers/v1/search.controller.ts
@@ -1,17 +1,28 @@
 import { Request, Response } from "express";
 
 import placeModel from "../../models/place.model.js";
+import { Op } from "sequelize";
 
 export const get = async (req: Request, res: Response): Promise<void> => {
   try {
     const { keyword } = req.params;
 
     // The keyword that the client sends is searched in the place descriptions.
-    const search = await placeModel.find({
-      description: { $regex: ".*" + keyword + ".*" },
+    // const search = await placeModel.find({
+    //   description: { $regex: ".*" + keyword + ".*" },
+    // });
+
+    const search = await placeModel.findAll({
+      where: {
+        description: { [Op.like]: `%${keyword}%` },
+      },
     });
 
-    res.status(200).json(search);
+    const searchResult = search.forEach((items) => {
+      return items.dataValues;
+    });
+
+    res.status(200).json(searchResult);
     return;
   } catch (error) {
     if (error) {
