7370db725a8316e22ae9b49b373b5f58a549ceb9
ME.AT <meatish2008@gmail.com>
2025-08-12 14:35:51 +0330

    feat(sequelizeConfig.ts): create config file for create database

---
+++ src/config/sequelizeConfig.ts
@@ -0,0 +1,28 @@
+const sequelizeConfig = {
+  development: {
+    username: process.env.DB_USERNAME!,
+    password: process.env.DB_PASSWORD!,
+    database: process.env.DB_DATABASE!,
+    host: process.env.DB_HOST!,
+    dialect: "mysql",
+    port: 3306,
+  },
+  test: {
+    username: process.env.DB_USERNAME!,
+    password: process.env.DB_PASSWORD!,
+    database: process.env.DB_DATABASE!,
+    host: process.env.DB_HOST!,
+    dialect: "mysql",
+    port: 3306,
+  },
+  production: {
+    username: process.env.DB_USERNAME!,
+    password: process.env.DB_PASSWORD!,
+    database: process.env.DB_DATABASE!,
+    host: process.env.DB_HOST!,
+    dialect: "mysql",
+    port: 3306,
+  },
+};
+
+export default sequelizeConfig;
