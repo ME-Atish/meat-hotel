8d1a4648b601d1d45593012a6dae1adc28493014
ME.AT <meatish2008@gmail.com>
2025-08-20 13:38:55 +0330

    start refactoring to nestjs

--- src/utils/generateRandomCode.ts
+++
@@ -1,5 +0,0 @@
-const generateRandomCode = () => {
-  return Math.floor(100000 + Math.random() * 900000).toString();
-};
-
-export default generateRandomCode;
--- src/utils/validators/email.validator.ts
+++
@@ -1,9 +0,0 @@
-const emailValidator = async (email: string) => {
-  const regexValidator = /^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gm;
-
-  const check = regexValidator.test(email);
-
-  return check;
-};
-
-export default emailValidator;
