7c5b824113f5a067ec742959828c5336e20fe4cf
ME.AT <meatish2008@gmail.com>
2025-08-20 14:40:25 +0330

    feat: create some basic things to run project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { WalletModule } from './wallet/wallet.module';
+import { PlaceModule } from './place/place.module';
+import { ReserveModule } from './reserve/reserve.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: `${process.env.DB_HOST}`,
+      port: 5432,
+      username: `${process.env.DB_USERNAME}`,
+      password: `${process.env.DB_PASSWORD}`,
+      database: `${process.env.DB_DATABASE}`,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    AuthModule,
+    PlaceModule,
+    WalletModule,
+    ReserveModule,
+  ],
 })
 export class AppModule {}
