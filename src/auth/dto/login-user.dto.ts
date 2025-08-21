d98b21c1634cd76d402924681cb42d5ba3a1817c
ME.AT <meatish2008@gmail.com>
2025-08-21 13:21:43 +0330

    feat(Login-user.dto.ts): create dto for login

---
+++ src/auth/dto/login-user.dto.ts
@@ -0,0 +1,24 @@
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class LoginUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(5)
+  @MaxLength(25)
+  identifier: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(5)
+  @MaxLength(16)
+  password: string;
+
+  @IsBoolean()
+  rememberMe: boolean;
+}
