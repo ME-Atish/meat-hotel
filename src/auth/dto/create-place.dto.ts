d17a9ace47cb22e390ebef2b089b13549401e03b
ME.AT <meatish2008@gmail.com>
2025-08-21 14:09:05 +0330

    feat(create-place.dto.ts): create dto for create place

---
+++ src/auth/dto/create-place.dto.ts
@@ -0,0 +1,42 @@
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreatePlaceDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(2)
+  @MaxLength(100)
+  name: string;
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  @MaxLength(100)
+  address: string;
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  @MaxLength(100)
+  description: string;
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(50)
+  facilities: string;
+  @IsNotEmpty()
+  @IsNumber()
+  price: number;
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(2)
+  @MaxLength(25)
+  province: string;
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(2)
+  @MaxLength(25)
+  city: string;
+}
